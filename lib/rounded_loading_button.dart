cd2c9f67a348f65e507e871f1ffb64e856a13da8
theaayushb <aayush.bhattarai@nexdigitalstudio.com>
2023-10-01 10:23:28 +0545

    resolved deprecated property

--- lib/rounded_loading_button.dart
+++ lib/rounded_loading_button.dart
@@ -203,12 +203,12 @@ class RoundedLoadingButtonState extends State<RoundedLoadingButton>
         ),
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-            onSurface: widget.disabledColor,
+            disabledForegroundColor: widget.disabledColor,
             minimumSize: Size(_squeezeAnimation.value, widget.height),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(widget.borderRadius),
             ),
-            primary: widget.color,
+            backgroundColor: widget.color,
             elevation: widget.elevation,
             padding: const EdgeInsets.all(0),
           ),
